1bd29df92451459cffb9772f427d68506db7a4c0
Krish Kashiwala <69708017+KrishKashiwala@users.noreply.github.com>
2021-10-05 07:32:19 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import threading
 import datetime
 
 host = "127.0.0.1"
-port = 6000
+port = 6001
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen(5)
@@ -20,17 +20,15 @@ def sendAllClients(msg, dontSendTo="None"):
 
 def onNewClient(client):
     name = client.recv(2040).decode()
-    loginTime = datetime.datetime.now().strftime("%X")
     allsockets[name] = client
-    loginMsg = "Server: time=" + loginTime + " " + name + " has joined. Member count=" + str(len(allsockets))
-    sendAllClients(loginMsg)
+    log_msg = name + " has joined. Total members : " + str(len(allsockets))
+    sendAllClients(log_msg)
     while True:
         chat = client.recv(2040).decode()
         if chat == "quit":
-            quitTime = datetime.datetime.now().strftime("%X")
-            quitmsg = "Server: time=" + quitTime + " " + name + " has left. Member count=" + str(len(allsockets) - 1)
+            quit_msg = name + " has left. Total members : " + str(len(allsockets) - 1)
             del allsockets[name]
-            sendAllClients(quitmsg)
+            sendAllClients(quit_msg)
             client.send("qqqqq".encode("UTF-8"))
             client.close()
             break
@@ -44,4 +42,3 @@ while True:
     t = threading.Thread(target=onNewClient, args=(client,))
     allthreads.append(t)
     t.start()
-
